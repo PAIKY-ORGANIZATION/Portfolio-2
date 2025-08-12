5aa2e996baf4f4ad6dcada01ff22a24c566f2ccb
Priapisman677 <expeliamm02@hotmail.com>
2025-08-12 08:51:46 -0700

    update

--- src/components/Social-links.tsx
+++ src/components/Social-links.tsx
@@ -4,8 +4,8 @@ import { MdContactPage } from "react-icons/md";
 
 
 export const socialLinks = [
+  { href: "https://drive.google.com/file/d/1Beoe-4M5fXmrXO_4N-1Xg_E5BA23FaEr/view?usp=sharing", label: "Resume", icon: <MdContactPage className="w-6 h-6 md:w-7 md:h-7 font-bold" /> },
   { href: "https://www.youtube.com/@CryptoComet-z1u", label: "Youtube", icon: <FaYoutube className="w-6 h-6 md:w-7 md:h-7 font-bold" /> },
   { href: "https://github.com/dev-Miguel-Mendez", label: "GitHub", icon: <FaGithub className="w-6 h-6 md:w-7 md:h-7 font-bold" /> },
   { href: "https://www.linkedin.com/in/miguel-mendez-g/", label: "LinkedIn", icon: <FaLinkedin className="w-6 h-6 md:w-7 md:h-7 font-bold" /> },
-  { href: "/", label: "Resume", icon: <MdContactPage className="w-6 h-6 md:w-7 md:h-7 font-bold" /> },
 ];

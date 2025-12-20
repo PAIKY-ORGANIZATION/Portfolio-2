6d4ca7b7a067d178b63f64e46c6a2a3c10c73952
paiky <dev-miguel-mendez@proton.me>
2025-12-20 11:36:55 -0700

    update

--- src/app/api/get-request-logs/route.ts
+++ src/app/api/get-request-logs/route.ts
@@ -7,7 +7,6 @@ export const GET = async()=>{
     const logs = fs.readFileSync(mainPageLogsPath, 'utf-8')
         .split('\n').slice(-200).join('\n'); //* Get the last 200 lines
 
-
     return new NextResponse(logs, {
         headers: {'Content-Type': 'text/plain'}
     })
